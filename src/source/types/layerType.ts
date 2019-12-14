acc04aab3989d8c9ae66fcc1d35b05fc67b74099
PUPIER Florent <florent.pupier@sword-group.com>
2019-12-14 11:55:24 +0100

    also export enum in source and layer type file

--- src/source/types/layerType.ts
+++ src/source/types/layerType.ts
@@ -1,1 +1,12 @@
-export type LayerType = 'Image' | 'Tile' | 'Vector' | 'VectorTile';
+export enum LayerTypeEnum {
+  Image = 'Image',
+  Tile = 'Tile',
+  Vector = 'Vector',
+  VectorTile = 'VectorTile',
+}
+
+export type LayerType =
+    | LayerTypeEnum.Image
+    | LayerTypeEnum.Tile
+    | LayerTypeEnum.Vector
+    | LayerTypeEnum.VectorTile;
--- src/source/types/sourceType.ts
+++ src/source/types/sourceType.ts
@@ -1,13 +1,28 @@
 export type SourceType =
-  | 'ExternalVector'
-  | 'ImageArcGISRest'
-  | 'ImageStatic'
-  | 'ImageWms'
-  | 'LocalVector'
-  | 'QueryArcGISRest'
-  | 'TileArcGISRest'
-  | 'TileWms'
-  | 'Vector'
-  | 'VectorTile'
-  | 'Wfs'
-  | 'Xyz';
+    | SourceTypeEnum.ExternalVector
+    | SourceTypeEnum.ImageArcGISRest
+    | SourceTypeEnum.ImageStatic
+    | SourceTypeEnum.ImageWms
+    | SourceTypeEnum.LocalVector
+    | SourceTypeEnum.QueryArcGISRest
+    | SourceTypeEnum.TileArcGISRest
+    | SourceTypeEnum.TileWms
+    | SourceTypeEnum.Vector
+    | SourceTypeEnum.VectorTile
+    | SourceTypeEnum.Wfs
+    | SourceTypeEnum.Xyz;
+
+export enum SourceTypeEnum {
+    ExternalVector = 'ExternalVector',
+    ImageArcGISRest = 'ImageArcGISRest',
+    ImageStatic = 'ImageStatic',
+    ImageWms = 'ImageWms',
+    LocalVector = 'LocalVector',
+    QueryArcGISRest = 'QueryArcGISRest',
+    TileArcGISRest = 'TileArcGISRest',
+    TileWms = 'TileWms',
+    Vector = 'Vector',
+    VectorTile = 'VectorTile',
+    Wfs = 'Wfs',
+    Xyz = 'Xyz',
+}
