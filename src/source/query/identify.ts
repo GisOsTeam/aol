363267999fc10f85d3b381c36e67c655e359ee66
dbelzunce <donatien.belzunce@sword-group.com>
2019-12-16 08:32:52 +0100

    move identify limit in params

--- src/source/query/identify.ts
+++ src/source/query/identify.ts
@@ -5,11 +5,11 @@ import Layer from 'ol/layer/Layer';
 import { IQueryResponse, constructQueryRequestFromPixel, IExtended } from '../IExtended';
 import { walk } from '../../utils';
 
-export function identify(pixel: Pixel, map: Map): Promise<IQueryResponse[]> {
+export function identify(pixel: Pixel, map: Map, limit: number = 10): Promise<IQueryResponse[]> {
   if (map && pixel) {
     const promises: Array<Promise<IQueryResponse>> = [];
     const queryRequest = constructQueryRequestFromPixel(pixel, 2, map);
-    queryRequest.limit = 10;
+    queryRequest.limit = limit;
 
     walk(map, (layer: OlBaseLayer) => {
       if (layer.getVisible() && 'getSource' in layer) {
