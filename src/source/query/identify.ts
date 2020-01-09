fd59ef60394354ba4ec03400896909d213f9b7fc
dbelzunce <donatien.belzunce@sword-group.com>
2020-01-09 17:31:14 +0100

    Ajout de l'OlLayer a l'IdentifyQueryResponseFeature

--- src/source/query/identify.ts
+++ src/source/query/identify.ts
@@ -6,24 +6,32 @@ import { IQueryResponse, constructQueryRequestFromPixel, IExtended } from '../IE
 import { walk } from '../../utils';
 
 export type IdentifyFilterType = (extended: IExtended) => boolean;
+export interface IIdentifyQueryResponse extends IQueryResponse {
+  olLayer: Layer;
+}
+
 export function identify(
   pixel: Pixel,
   map: Map,
   limit: number = 10,
   filter?: IdentifyFilterType
-): Promise<IQueryResponse[]> {
+): Promise<IIdentifyQueryResponse[]> {
   if (map && pixel) {
-    const promises: Array<Promise<IQueryResponse>> = [];
+    const promises: Array<Promise<IIdentifyQueryResponse>> = [];
     const queryRequest = constructQueryRequestFromPixel(pixel, 2, map);
     queryRequest.limit = limit;
 
     walk(map, (layer: OlBaseLayer) => {
       if (layer.getVisible() && 'getSource' in layer) {
-        const source = (layer as Layer).getSource();
+        const olLayer = layer as Layer;
+        const source = olLayer.getSource();
         if (source && 'query' in source) {
           const extended = source as IExtended;
           if (!filter || filter(extended)) {
-            promises.push(extended.query(queryRequest));
+            promises.push(extended.query(queryRequest).then((queryResponse: IQueryResponse) => {
+              const identifyQueryResponse = {...queryResponse, olLayer};
+              return identifyQueryResponse;
+            }));
           }
         }
       }
