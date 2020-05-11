9cf04bee7503c3cac9abb7ffb1887808ba8cf47b
fpupier <florent.pupier@sword-group.com>
2020-05-11 09:54:41 +0200

    MAJ des T-U

--- src/__tests__/source/wfs.ts
+++ src/__tests__/source/wfs.ts
@@ -13,7 +13,7 @@ describe('aol.wfs', () => {
     const projection = new Projection({ code: 'EPSG:4326' });
     const wfsSource = new Wfs(options);
     expect((wfsSource.getUrl() as FeatureUrlFunction)([0, 0, 0, 0], 0, projection)).toEqual(
-      'https://dservices2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/services/Seattle_Downtown_Features/WFSServer?service=WFS&version=2.0.0&request=GetFeature&Type=Seattle_Downtown_Features:Buildings&outputFormat=GEOJSON&srsname=EPSG:4326&bbox=0,0,0,0,EPSG:4326'
+      'https://dservices2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/services/Seattle_Downtown_Features/WFSServer?service=WFS&version=2.0.0&request=GetFeature&TypeName=Seattle_Downtown_Features:Buildings&outputFormat=GEOJSON&srsname=EPSG:4326&bbox=0,0,0,0,EPSG:4326'
     );
   });
 });
