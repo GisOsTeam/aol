fcdb069d2deecc189700e9ead41692f7be3d57d9
fpupier <florent.pupier@sword-group.com>
2021-03-25 15:23:54 +0100

    add hash function into util

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -1,32 +1,44 @@
-import { revertCoordinate } from '../utils';
+import { hash64, revertCoordinate } from '../utils';
 import Point from 'ol/geom/Point';
 import LinearRing from 'ol/geom/LinearRing';
 
-test('revert Point coordinate', () => {
-  const geometryToBeReverted = new Point([0, 1]);
-  expect<number[]>(geometryToBeReverted.getCoordinates()).toEqual([0, 1]);
-  revertCoordinate(geometryToBeReverted);
-  expect<number[]>(geometryToBeReverted.getCoordinates()).toEqual([1, 0]);
+describe('utils', () => {
+  test('revert Point coordinate', () => {
+    const geometryToBeReverted = new Point([0, 1]);
+    expect<number[]>(geometryToBeReverted.getCoordinates()).toEqual([0, 1]);
+    revertCoordinate(geometryToBeReverted);
+    expect<number[]>(geometryToBeReverted.getCoordinates()).toEqual([1, 0]);
+  });
+
+  test('revert LinearRing coordinate', () => {
+    const geometryToBeReverted = new LinearRing([
+      [0, 1],
+      [1, 1],
+      [1, 2],
+      [2, 2],
+    ]);
+    expect<number[][]>(geometryToBeReverted.getCoordinates()).toEqual([
+      [0, 1],
+      [1, 1],
+      [1, 2],
+      [2, 2],
+    ]);
+    revertCoordinate(geometryToBeReverted);
+    expect<number[][]>(geometryToBeReverted.getCoordinates()).toEqual([
+      [1, 0],
+      [1, 1],
+      [2, 1],
+      [2, 2],
+    ]);
+  });
+
+  test('hash64', () => {
+    testHash('a', hash64);
+    testHash('&', hash64);
+    testHash('{ }', hash64);
+  });
 });
 
-test('revert LinearRing coordinate', () => {
-  const geometryToBeReverted = new LinearRing([
-    [0, 1],
-    [1, 1],
-    [1, 2],
-    [2, 2],
-  ]);
-  expect<number[][]>(geometryToBeReverted.getCoordinates()).toEqual([
-    [0, 1],
-    [1, 1],
-    [1, 2],
-    [2, 2],
-  ]);
-  revertCoordinate(geometryToBeReverted);
-  expect<number[][]>(geometryToBeReverted.getCoordinates()).toEqual([
-    [1, 0],
-    [1, 1],
-    [2, 1],
-    [2, 2],
-  ]);
-});
+function testHash(s: string, hashFn: (s: string) => string | number): void {
+  expect(hashFn(`${s}`)).toEqual(hashFn(s));
+}
