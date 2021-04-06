f3ed7184fc37ff49fc23ed4ca06d94df182f3057
fpupier <florent.pupier@sword-group.com>
2021-04-06 15:21:42 +0200

    lint

--- src/source/query/wfs.ts
+++ src/source/query/wfs.ts
@@ -165,7 +165,10 @@ export function executeWfsQuery(options: {
     // Calcul de la hauteur géo référencée comprenant la hauteur de l'étendue originale et 2 * la tolérance (haut et bas)
     const geoHeight = Math.abs(originalExtent[3] - originalExtent[1]) + 2 * geoTolerance;
     // Calcul du centre de l'étendue originale
-    const originalExtentCenter = [0.5 * originalExtent[0] + 0.5 * originalExtent[2], 0.5 * originalExtent[1] + 0.5 * originalExtent[3]];
+    const originalExtentCenter = [
+      0.5 * originalExtent[0] + 0.5 * originalExtent[2],
+      0.5 * originalExtent[1] + 0.5 * originalExtent[3],
+    ];
     // Calcule de l'étendue intégrant la tolérance
     const extentBuffered = getForViewAndSize(
       originalExtentCenter,
