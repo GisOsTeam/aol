7a5c1a8aaec663b63d80cde8259c5ef4d0c55500
fpupier <florent.pupier@sword-group.com>
2021-09-22 17:10:41 +0200

    v0.2.3

--- src/filter/predicate/BasePredicate.ts
+++ src/filter/predicate/BasePredicate.ts
@@ -7,7 +7,8 @@ export abstract class BasePredicate<
   LH extends IPredicate | IField<any>,
   O extends IOperator,
   RH extends IPredicate | FilterValueType
-> implements IPredicate<LH, O, RH> {
+> implements IPredicate<LH, O, RH>
+{
   public readonly leftHand: LH;
   public readonly operator: O;
   public readonly rightHand: RH;
