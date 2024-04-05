fd17774f529b3cfaa7f0a1993a85d77890876e09
Mathieu Mast <mathieu.mast>
2024-04-05 15:10:58 +0200

    Add LayerTypeEnum.Heatmap

--- src/source/types/layerType.ts
+++ src/source/types/layerType.ts
@@ -2,8 +2,15 @@ export enum LayerTypeEnum {
   Image = 'Image',
   Tile = 'Tile',
   WebGLTile = 'WebGLTile',
+  Heatmap = 'Heatmap',
   Vector = 'Vector',
   VectorTile = 'VectorTile',
 }
 
-export type LayerType = LayerTypeEnum.Image | LayerTypeEnum.Tile | LayerTypeEnum.WebGLTile | LayerTypeEnum.Vector | LayerTypeEnum.VectorTile;
+export type LayerType =
+  | LayerTypeEnum.Image
+  | LayerTypeEnum.Tile
+  | LayerTypeEnum.WebGLTile
+  | LayerTypeEnum.Heatmap
+  | LayerTypeEnum.Vector
+  | LayerTypeEnum.VectorTile;
