aa39de794c5147b9dad697ada32381f2a72fbbe2
fpupier <florent.pupier@coexya.eu>
2026-02-05 19:51:45 +0000

    refactor(WmtsProvider): streamline capabilities options handling in provideSync and provideAsync methods

--- src/source/provider/WmtsProvider.ts
+++ src/source/provider/WmtsProvider.ts
@@ -1,27 +1,25 @@
 import { Wmts } from '../Wmts';
 import { WmtsFactory } from '../factory/WmtsFactory';
 import { IWmtsCapabilitiesOptions } from '../WmtsCapabilities';
-import { HttpEngine } from '../../HttpEngine';
+import { HttpEngine, IHttpRequest } from '../../HttpEngine';
 
 export class WmtsProvider {
   public static provideSync(
     source: Document | Element | string,
     wmtsCapabilitiesOptions: IWmtsCapabilitiesOptions,
   ): Wmts {
-    this.isWmtsCapabilitiesOptionsValid(wmtsCapabilitiesOptions);
-    return WmtsFactory.create(source, wmtsCapabilitiesOptions);
+    return WmtsFactory.create(source, this.sanetizeWmtsCapabilitiesOptions(wmtsCapabilitiesOptions));
   }
 
   public static provideAsync(wmtsCapabilitiesOptions: IWmtsCapabilitiesOptions): Promise<Wmts> {
-    this.isWmtsCapabilitiesOptionsValid(wmtsCapabilitiesOptions);
-    if (!wmtsCapabilitiesOptions.capabilitiesUrl) {
-      wmtsCapabilitiesOptions.capabilitiesUrl = `${wmtsCapabilitiesOptions.url}?SERVICE=WMTS&REQUEST=GetCapabilities&VERSION=1.0.0`;
-    }
-    const request = {
-      url: wmtsCapabilitiesOptions.capabilitiesUrl,
+    const _sanetizedOptions = this.sanetizeWmtsCapabilitiesOptions(wmtsCapabilitiesOptions);
+
+    const request: IHttpRequest = {
+      url: _sanetizedOptions.capabilitiesUrl,
       method: 'GET',
       responseType: 'text',
     };
+
     return HttpEngine.getInstance()
       .send(request)
       .then((response) => {
@@ -30,14 +28,23 @@ export class WmtsProvider {
         capabilitiesTxt = capabilitiesTxt.replace(/urn:ogc:def:crs:EPSG:[0-9.]*:([0-9]+)/gi, 'EPSG:$1');
         capabilitiesTxt = capabilitiesTxt.replace(/urn:ogc:def:crs:OGC:[0-9.]*:(CRS)?([0-9]+)/gi, 'CRS:$2');
         // FIN HACK
-        return WmtsFactory.create(capabilitiesTxt, wmtsCapabilitiesOptions);
+        return WmtsFactory.create(capabilitiesTxt, _sanetizedOptions);
       });
   }
 
-  private static isWmtsCapabilitiesOptionsValid(wmtsCapabilitiesOptions: IWmtsCapabilitiesOptions): boolean {
+  private static sanetizeWmtsCapabilitiesOptions(
+    wmtsCapabilitiesOptions: IWmtsCapabilitiesOptions,
+  ): IWmtsCapabilitiesOptions {
+    const _sanetizedOptions = { ...wmtsCapabilitiesOptions };
     if (!wmtsCapabilitiesOptions.layer) {
       throw new Error(`WmtsCapabilitiesOptions.layer is mandatory to provide wmts.`);
     }
-    return true;
+    if (!wmtsCapabilitiesOptions.version) {
+      _sanetizedOptions.version = '1.0.0';
+    }
+    if (!wmtsCapabilitiesOptions.capabilitiesUrl) {
+      _sanetizedOptions.capabilitiesUrl = `${wmtsCapabilitiesOptions.url}?SERVICE=WMTS&REQUEST=GetCapabilities&VERSION=${_sanetizedOptions.version}`;
+    }
+    return _sanetizedOptions;
   }
 }
